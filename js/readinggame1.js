8adfe1074d384a217aa9fa76d76b4596b81e23ef
Jesse Monroy <jessemonroy650@yahoo.com>
2015-03-15 01:51:16 -0700

    .

--- js/readinggame1.js
+++ js/readinggame1.js
@@ -1,96 +1,4 @@
 //
-// Audio player
-//
-//  http://plugins.cordova.io/#/package/org.apache.cordova.media
-//  == Constants ==
-//  Media.MEDIA_NONE = 0;
-//  Media.MEDIA_STARTING = 1;
-//  Media.MEDIA_RUNNING = 2;
-//  Media.MEDIA_PAUSED = 3;
-//  Media.MEDIA_STOPPED = 4;
-//
-//  MediaError.MEDIA_ERR_ABORTED = 1
-//  MediaError.MEDIA_ERR_NETWORK = 2
-//  MediaError.MEDIA_ERR_DECODE = 3
-//  MediaError.MEDIA_ERR_NONE_SUPPORTED = 4
-//
-
-var PhoneGapAudio = {
-	elementId  : undefined,
-	my_media   : null,
-	mediaTimer : null,
-
-
-	init : function (id) {
-	},
-	//
-	// Play audio
-    //
-	playAudio : function (src) {
-		if (this.my_media === null) {
-			// Create Media object from src
-			this.my_media = new Media(src, onSuccess, onError);
-		} // else play current audio
-		// Play audio
-		this.my_media.play();
-		// Update my_media position every second
-		if (this.mediaTimer === null) {
-			this.mediaTimer = setInterval(function() {
-				// get my_media position
-				this.my_media.getCurrentPosition(
-					// success callback
-					function(position) {
-						if (position > -1) {setAudioPosition((position) + " sec");}
-					},
-					// error callback
-					function(e) {
-						console.log("Error getting pos=" + e);
-						setAudioPosition("Error: " + e);
-					}
-				);
-			}, 1000);
-		}
-	},
-	//
-	// Pause audio
-	// 
-	pauseAudio : function () {
-		if (this.my_media) {
-			this.my_media.pause();
-		}
-	},
-	//
-	// Stop audio
-	// 
-	stopAudio : function () {
-		if (this.my_media) {
-			this.my_media.stop();
-		}
-		clearInterval(this.mediaTimer);
-		this.mediaTimer = null;
-	},
-	//
-	// Set audio position
-	// 
-	setAudioPosition : function (position) {
-		if (document.getElementById('audio_position')) {
-			document.getElementById('audio_position').innerHTML = position;
-		}
-	},
-	//
-	// onSuccess Callback
-	//
-	onSuccess : function () {
-		console.log("playAudio():Audio Success");
-	},
-	//
-	// onError Callback 
-	//
-	onError : function (error) {
-		alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-	}
-};
-//
 //  Reading California Game #1 - Choose Letter Bingo
 //  by Jesus Monroy, Jr.
 //  Date: 2015-03-13
@@ -159,7 +67,7 @@ var ReadingGame1 = {
 
 	    src = this.soundsDirectory + this.sounds[sel];
 		//console.log("gfPlayAudio(src):" + src)
-		//alert("gfPlayAudio(src):" + src);
+		alert("gfPlayAudio(src):" + src);
 	    PhoneGapAudio.playAudio(src);
 		alert("snd");
 	},
